bb0b60f3de6584cd0b3cc737de7dc50dbdf34f38
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-10-28 22:27:50 -0500

    Initial go at parsing json

--- urls_from_xbs_json.py
+++ urls_from_xbs_json.py
@@ -21,3 +21,37 @@ with open(args.input, "r") as inputFile:
     all_bookmarks_raw = inputFile.read()
 
 all_bookmarks = json.loads(all_bookmarks_raw)
+
+#todo:
+#   - filter via regex rather then "in"
+#   - how to return list rather then print
+#   - input blacklist names via args
+
+blacklist_names = ['[xbs] Other', 'TestA']
+
+def get_filter(bookmarks, blacklist_names):
+    
+    if isinstance(bookmarks, dict):
+        if "children" in bookmarks.keys():
+            if bookmarks['title'] not in blacklist_names:
+                get_filter(bookmarks['children'], blacklist_names)
+            if "url" in bookmarks.keys():
+                print('found url in children dict')
+                raise
+        elif "url" in bookmarks.keys():
+            print(bookmarks['url'])
+        else:
+            print('did not find children or url in dict')
+            raise
+    elif isinstance(bookmarks, list):
+        for item in bookmarks:
+            get_filter(item, blacklist_names)
+    else:
+        print("not sure how you got here, type: ")
+        print(type(bookmarks))
+
+get_filter(all_bookmarks, blacklist_names)
+
+
+
+
