75f0f668be7bb63bd1265a0a7854253e6d245e37
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-10-29 10:30:34 -0500

    return urls, and regex fullmatch

--- urls_from_xbs_json.py
+++ urls_from_xbs_json.py
@@ -2,6 +2,7 @@
 
 import json
 import argparse
+import re
 
 #Setup args
 parser = argparse.ArgumentParser(
@@ -23,35 +24,40 @@ with open(args.input, "r") as inputFile:
 all_bookmarks = json.loads(all_bookmarks_raw)
 
 #todo:
-#   - filter via regex rather then "in"
-#   - how to return list rather then print
 #   - input blacklist names via args
 
-blacklist_names = ['[xbs] Other', 'TestA']
+blacklist_names = ['\[xbs\] Other', 'TestA', 'writing']
+blacklist_regex = '(?:% s)' % '|'.join(blacklist_names) 
+#print(blacklist_regex)
 
-def get_filter(bookmarks, blacklist_names):
-    
+def filter_bookmarks(bookmarks, blacklist_regex):
+    urls = []
     if isinstance(bookmarks, dict):
         if "children" in bookmarks.keys():
-            if bookmarks['title'] not in blacklist_names:
-                get_filter(bookmarks['children'], blacklist_names)
+            match = re.fullmatch(blacklist_regex, bookmarks['title'])
+            if not match:
+                urls.extend(filter_bookmarks(bookmarks['children'], blacklist_regex))
+            else:
+                print('skipping folder ' + bookmarks['title'])
             if "url" in bookmarks.keys():
                 print('found url in children dict')
                 raise
         elif "url" in bookmarks.keys():
-            print(bookmarks['url'])
+            urls.append(bookmarks['url'])
         else:
             print('did not find children or url in dict')
             raise
     elif isinstance(bookmarks, list):
         for item in bookmarks:
-            get_filter(item, blacklist_names)
+            urls.extend(filter_bookmarks(item, blacklist_regex))
     else:
         print("not sure how you got here, type: ")
         print(type(bookmarks))
+    return urls
 
-get_filter(all_bookmarks, blacklist_names)
+urls = filter_bookmarks(all_bookmarks, blacklist_regex)
+#urls = [item for sublist in nested_urls for item in sublist]
 
-
+#print(*urls, sep = "\n") 
 
 
